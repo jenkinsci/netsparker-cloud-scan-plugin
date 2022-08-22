21b328fba68908e40f2ca37a90b4a6fe247825fb
KeremOzben <kerem.ozben@invicti.com>
2022-08-22 14:55:04 +0300

    jenkins & plugin updates & removed "please select" options

--- src/main/java/com/netsparker/cloud/plugin/NCScanBuilder.java
+++ src/main/java/com/netsparker/cloud/plugin/NCScanBuilder.java
@@ -557,7 +557,6 @@ public class NCScanBuilder extends Builder implements SimpleBuildStep {
         @SuppressWarnings("unused")
         public ListBoxModel doFillNcScanTypeItems() {
             ListBoxModel model = new ListBoxModel();
-            model.add("-- Please select a scan type --", "");
             model.add("Incremental", "Incremental");
             model.add("Full (With primary profile)", "FullWithPrimaryProfile");
             model.add("Full (With selected profile)", "FullWithSelectedProfile");
@@ -573,9 +572,7 @@ public class NCScanBuilder extends Builder implements SimpleBuildStep {
             }
 
             ListBoxModel model = new ListBoxModel();
-            if (model.isEmpty()) {
-                model.add("-- Please select a website --", "");
-            }
+            
             for (WebsiteModel websiteModel : websiteModels) {
                 model.add(websiteModel.getDisplayName(), websiteModel.getId());
             }
@@ -595,14 +592,11 @@ public class NCScanBuilder extends Builder implements SimpleBuildStep {
 
             String placeholderText;
             final ArrayList<WebsiteProfileModel> websiteProfileModels = websiteModel.getProfiles();
+            ListBoxModel model = new ListBoxModel();
             if (websiteProfileModels.isEmpty()) {
                 placeholderText = "-- No profile found --";
-            } else {
-                placeholderText = "-- Please select a profile name --";
+                model.add(placeholderText, "");
             }
-
-            ListBoxModel model = new ListBoxModel();
-            model.add(placeholderText, "");
 
             for (WebsiteProfileModel websiteProfileModel : websiteProfileModels) {
                 model.add(websiteProfileModel.getName(), websiteProfileModel.getId());
